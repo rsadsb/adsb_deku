528043c5b1c4de3cf8da47ed0e8c955d52410f63
John Wiseman <jjwiseman@gmail.com>
2021-10-14 17:27:12 -0700

    Added --host and --port to 1090 app.

--- apps/src/1090.rs
+++ apps/src/1090.rs
@@ -1,13 +1,29 @@
 use adsb_deku::deku::DekuContainerRead;
 use adsb_deku::{Frame, DF, ME};
 
+use clap::{AppSettings, Clap};
 use std::io::{BufRead, BufReader};
 use std::net::TcpStream;
 
 use apps::Airplanes;
 
+#[derive(Debug, Clap)]
+#[clap(version = "1.0", author = "wcampbell <wcampbell1995@gmail.com>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Options {
+    #[clap(long)]
+    host: Option<String>,
+    #[clap(long)]
+    port: Option<u16>,
+}
+
 fn main() {
-    let stream = TcpStream::connect(("127.0.0.1", 30002)).unwrap();
+    let options = Options::parse();
+    let stream = TcpStream::connect((
+        options.host.unwrap_or_else(|| "localhost".to_string()),
+        options.port.unwrap_or(30002),
+    ))
+    .unwrap();
     let mut reader = BufReader::new(stream);
     let mut input = String::new();
     let mut airplains = Airplanes::new();
