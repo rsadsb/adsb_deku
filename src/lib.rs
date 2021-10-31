c97b0f62d48fe0b4f6ac961faca56cdf3ff3498c
wcampbell <wcampbell1995@gmail.com>
2021-10-31 01:38:51 -0400

    move mode_ac to module
    Signed-off-by: wcampbell <wcampbell1995@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -127,6 +127,7 @@ pub mod adsb;
 pub mod bds;
 pub mod cpr;
 mod crc;
+mod mode_ac;
 
 use adsb::{ControlField, ADSB};
 use bds::BDS;
@@ -556,122 +557,6 @@ impl std::fmt::Display for Sign {
                 Self::Negative => "-",
             }
         )
-    }
-}
-
-mod mode_ac {
-    pub fn decode_id13_field(id13_field: u32) -> u32 {
-        let mut hex_gillham: u32 = 0;
-
-        if id13_field & 0x1000 != 0 {
-            hex_gillham |= 0x0010;
-        } // Bit 12 = C1
-        if id13_field & 0x0800 != 0 {
-            hex_gillham |= 0x1000;
-        } // Bit 11 = A1
-        if id13_field & 0x0400 != 0 {
-            hex_gillham |= 0x0020;
-        } // Bit 10 = C2
-        if id13_field & 0x0200 != 0 {
-            hex_gillham |= 0x2000;
-        } // Bit  9 = A2
-        if id13_field & 0x0100 != 0 {
-            hex_gillham |= 0x0040;
-        } // Bit  8 = C4
-        if id13_field & 0x0080 != 0 {
-            hex_gillham |= 0x4000;
-        } // Bit  7 = A4
-          //if id13_field & 0x0040 != 0 {hex_gillham |= 0x0800;} // Bit  6 = X  or M
-        if id13_field & 0x0020 != 0 {
-            hex_gillham |= 0x0100;
-        } // Bit  5 = B1
-        if id13_field & 0x0010 != 0 {
-            hex_gillham |= 0x0001;
-        } // Bit  4 = D1 or Q
-        if id13_field & 0x0008 != 0 {
-            hex_gillham |= 0x0200;
-        } // Bit  3 = B2
-        if id13_field & 0x0004 != 0 {
-            hex_gillham |= 0x0002;
-        } // Bit  2 = D2
-        if id13_field & 0x0002 != 0 {
-            hex_gillham |= 0x0400;
-        } // Bit  1 = B4
-        if id13_field & 0x0001 != 0 {
-            hex_gillham |= 0x0004;
-        } // Bit  0 = D4
-
-        hex_gillham
-    }
-
-    pub fn mode_a_to_mode_c(mode_a: u32) -> Result<u32, &'static str> {
-        let mut five_hundreds: u32 = 0;
-        let mut one_hundreds: u32 = 0;
-
-        // check zero bits are zero, D1 set is illegal; C1,,C4 cannot be Zero
-        if (mode_a & 0xffff_8889) != 0 || (mode_a & 0x0000_00f0) == 0 {
-            return Err("Invalid altitude");
-        }
-
-        if mode_a & 0x0010 != 0 {
-            one_hundreds ^= 0x007;
-        } // C1
-        if mode_a & 0x0020 != 0 {
-            one_hundreds ^= 0x003;
-        } // C2
-        if mode_a & 0x0040 != 0 {
-            one_hundreds ^= 0x001;
-        } // C4
-
-        // Remove 7s from OneHundreds (Make 7->5, snd 5->7).
-        if (one_hundreds & 5) == 5 {
-            one_hundreds ^= 2;
-        }
-
-        // Check for invalid codes, only 1 to 5 are valid
-        if one_hundreds > 5 {
-            return Err("Invalid altitude");
-        }
-
-        // if mode_a & 0x0001 {five_hundreds ^= 0x1FF;} // D1 never used for altitude
-        if mode_a & 0x0002 != 0 {
-            five_hundreds ^= 0x0ff;
-        } // D2
-        if mode_a & 0x0004 != 0 {
-            five_hundreds ^= 0x07f;
-        } // D4
-
-        if mode_a & 0x1000 != 0 {
-            five_hundreds ^= 0x03f;
-        } // A1
-        if mode_a & 0x2000 != 0 {
-            five_hundreds ^= 0x01f;
-        } // A2
-        if mode_a & 0x4000 != 0 {
-            five_hundreds ^= 0x00f;
-        } // A4
-
-        if mode_a & 0x0100 != 0 {
-            five_hundreds ^= 0x007;
-        } // B1
-        if mode_a & 0x0200 != 0 {
-            five_hundreds ^= 0x003;
-        } // B2
-        if mode_a & 0x0400 != 0 {
-            five_hundreds ^= 0x001;
-        } // B4
-
-        // Correct order of one_hundreds.
-        if five_hundreds & 1 != 0 && one_hundreds <= 6 {
-            one_hundreds = 6 - one_hundreds;
-        }
-
-        let n = (five_hundreds * 5) + one_hundreds;
-        if n >= 13 {
-            Ok(n - 13)
-        } else {
-            Err("Invalid altitude")
-        }
     }
 }
 
